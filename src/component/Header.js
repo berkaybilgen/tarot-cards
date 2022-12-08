48154f344d05cd62afa3d565535616947b32e9fd
Berkay <berkaybilgen@yandex.com>
2022-12-08 23:12:30 +0300

    selected menu item is highlighted

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,30 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 const Header = (props) => {
+  const [selectedListItem, setSelectedListItem] = useState(0);
+
   const filterCardsHandler = (event) => {
+    setSelectedListItem(Number(event.target.dataset.cat));
     props.changeSelectedCat(Number(event.target.dataset.cat));
   };
 
   return (
     <header>
       <ul>
-        <li onClick={filterCardsHandler} data-cat="0">
+        <li
+          className={selectedListItem === 0 ? "selected" : ""}
+          onClick={filterCardsHandler}
+          data-cat="0"
+        >
           All
         </li>
-        <li onClick={filterCardsHandler} data-cat="1">
+        <li
+          className={selectedListItem === 1 ? "selected" : ""}
+          onClick={filterCardsHandler}
+          data-cat="1"
+        >
           Major Arcana
         </li>
-        <li onClick={filterCardsHandler} data-cat="2">
+        <li
+          className={selectedListItem === 2 ? "selected" : ""}
+          onClick={filterCardsHandler}
+          data-cat="2"
+        >
           Wands
         </li>
-        <li onClick={filterCardsHandler} data-cat="3">
+        <li
+          className={selectedListItem === 3 ? "selected" : ""}
+          onClick={filterCardsHandler}
+          data-cat="3"
+        >
           Cups
         </li>
-        <li onClick={filterCardsHandler} data-cat="4">
+        <li
+          className={selectedListItem === 4 ? "selected" : ""}
+          onClick={filterCardsHandler}
+          data-cat="4"
+        >
           Swords
         </li>
-        <li onClick={filterCardsHandler} data-cat="5">
+        <li
+          className={selectedListItem === 5 ? "selected" : ""}
+          onClick={filterCardsHandler}
+          data-cat="5"
+        >
           Pentacles
         </li>
       </ul>
