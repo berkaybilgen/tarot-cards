5210ccf4a69cd8e69419f530624e6d76a4773073
Berkay <berkaybilgen@yandex.com>
2022-12-09 23:04:38 +0300

    Added progressive image loading

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,12 +1,16 @@
 import React from "react";
+import ProgressiveImage from "react-progressive-graceful-image";
 import "./Card.css";
 
 const Card = (props) => {
   let image = "/tarot-cards/cards/" + props.no + ".jpg";
+  let tempImage = "/tarot-cards/cards/" + props.no + "_temp.jpg";
 
   return (
     <div className="card">
-      <img src={image} />
+      <ProgressiveImage src={image} placeholder={tempImage}>
+        {(src) => <img src={src} alt={props.name} />}
+      </ProgressiveImage>
     </div>
   );
 };
